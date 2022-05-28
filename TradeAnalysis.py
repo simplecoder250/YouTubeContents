f6c93122739181c7871f6131391cbfbe95579ffb
simplecoder250 <76175237+simplecoder250@users.noreply.github.com>
2022-05-29 00:06:44 +0530

    Changes

--- TradeAnalysis.py
+++ TradeAnalysis.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 TCSData = pd.read_csv('TCS.csv')
@@ -20,7 +19,6 @@ for i in range(len(TCSData)) :
             print(count)
             MaxLossIndex = i
             print(TCSData.loc[i,"entry_time"])
-            print('\n')
         count = 0
 
 print("Maximum loss days->",maximumLossDays)
@@ -44,36 +42,6 @@ for i in range(len(TCSData)) :
 
 print("Maximum profit days->",maximumProfitDays)
 
-# Maximum consecutive loss amount
-
-startOfMaxProfitTime = ( MaxProfitIndex - maximumProfitDays + 1 )
-count=0
-maxProfitValue = 0
-for i in range(len(TCSData)) :
-    if ( i == startOfMaxProfitTime) :
-        count = count + 1
-        maxProfitValue = maxProfitValue + TCSData.loc[i,"pnl"]
-
-        if (count == maximumProfitDays) :
-            break
-
-print("Maximum Profit Value is :",maxProfitValue)
-
-# Maximum consecutive loss amount
-count = 0
-maxLossValue = 0
-startOfMaxLossTime = ( MaxLossIndex - maximumLossDays + 1)
-for i in range(len(TCSData)):
-    if (i == startOfMaxLossTime):
-        count = count + 1
-        maxLossValue = maxLossValue + TCSData.loc[i, "pnl"]
-
-        if (count == maximumLossDays):
-            break
-
-print("Maximum Profit Value is :", maxLossValue)
-
-
 
 # Calculate the max loss and profit return in a day in ( % )
 
@@ -90,6 +58,3 @@ for i in range(len(TCSData)):
 
 print("Maximum profit percentage :",maxProfitPerecentage)
 print("Minimum loss percentage :",maxLossPerecentage)
-
-
-
